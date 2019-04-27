a48d831e721e8b53d48edd094a16caf2c1ed036a
Krondini <koro5940@colorado.edu>
2019-04-27 00:15:57 -0600

    NOW I Finished Part 2 of HW4

--- HW4/HW4/Question/StudentWebsite/js/index.js
+++ HW4/HW4/Question/StudentWebsite/js/index.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {    
-    var apiKey = "" // Enter your API Key here        
-    console.log(`state_info is: ${state_info}`) // Notice the templating here, use that when you form your url
+    var apiKey = "cf52254d8bd072d33e99a0492f99721b"; // Enter your API Key here        
+    console.log(`state_info is: ${state_info}`); // Notice the templating here, use that when you form your url
 
 
     // TODO
     // Iterate over the state_info array and call the api for each state_name to get the current temperature
     // Example to call the api using state_name
     // This should be done inside the for loop
-    var state_obj = state_info['CO']
-    var url =`https://api.darksky.net/forecast/-apikey-/-latitude-,-longitude-`;
+    var state_obj = state_info['CO'];
+    var url =`https://api.darksky.net/forecast/cf52254d8bd072d33e99a0492f99721b/37.8267,-122.4233`;
 
     $.ajax({url:url, dataType:"jsonp"}).then(function(data) {
                 
-                console.log(data)
-                var temperature = null
+                console.log(data);
+                var temperature = null;
                 // TODO
                 // Fill in the RHS of the below line and uncomment it. Remember how we accessed the temperature in Lab 9.
                 // var temperature = 
 
-                console.log(temperature)
+                console.log(temperature);
 
                 //TODO 
                 // Default color gray
@@ -29,6 +29,7 @@ $(document).ready(function() {
                 // Between 51 and 80 should be orange
                 // Greater than 80 should be red
 
-                $('#CO').css('fill', "blue");   // Example on how to fill colors for your state.    
+                $('#CO').css("fill", "blue");   // Example on how to fill colors for your state.    
+                
     });
 });
